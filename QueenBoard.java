c6a67afde430d1301f6b0ac461db18386123ae79
Jessica Kim <jkim00@stuy.edu>
2019-02-07 09:58:07 -0500

    constructor, toString done

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,37 +1,61 @@
 public class QueenBoard {
   private int[][] board;
 
-  public QueenBoard(int size){}
+  public QueenBoard(int size){
+    board = new int[size][size];
+  }
 
-  private boolean addQueen(int r, int c) {}
+  public boolean addQueen(int r, int c) {
+    if (board[r][c] > 0) return false;
+    else {
+      board[r][c]++;
+      return true;
+    }
+  }
 
-  private boolean removeQueen(int r, int c){}
+  public boolean removeQueen(int r, int c){
+    if (board[r][c] == 0) return false;
+    else {
+      board[r][c]--;
+      return true;
+    }
+  }
 
-  /**
-  *@return The output string formatted as follows:
-  *All numbers that represent queens are replaced with 'Q'
-  *all others are displayed as underscores '_'
-  *There are spaces between each symbol:
-  *"""_ _ Q _
-  *Q _ _ _
-  *_ _ _ Q
-  *_ Q _ _"""
-  *(pythonic string notation for clarity,
-  *excludes the character up to the *)
-  */
-
-  public String toString(){}
-  /**
-  *@return false when the board is not solveable and leaves the board filled with zeros;
-  *        true when the board is solveable, and leaves the board in a solved state
-  *@throws IllegalStateException when the board starts with any non-zero value
-  */
-  public boolean solve(){}
-
-  /**
-  *@return the number of solutions found, and leaves the board filled with only 0's
-  *@throws IllegalStateException when the board starts with any non-zero value
-  */
-  public int countSolutions(){}
+  // /**
+  // *@return The output string formatted as follows:
+  // *All numbers that represent queens are replaced with 'Q'
+  // *all others are displayed as underscores '_'
+  // *There are spaces between each symbol:
+  // *"""_ _ Q _
+  // *Q _ _ _
+  // *_ _ _ Q
+  // *_ Q _ _"""
+  // *(pythonic string notation for clarity,
+  // *excludes the character up to the *)
+  // */
+  //
+  public String toString(){
+    String s = "";
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[0].length; j++) {
+        if (board[i][j] == 0) s+="_ ";
+        else s+="Q ";
+      }
+      s+="\n";
+    }
+    return s;
+  }
+  // /**
+  // *@return false when the board is not solveable and leaves the board filled with zeros;
+  // *        true when the board is solveable, and leaves the board in a solved state
+  // *@throws IllegalStateException when the board starts with any non-zero value
+  // */
+  // public boolean solve(){}
+  //
+  // /**
+  // *@return the number of solutions found, and leaves the board filled with only 0's
+  // *@throws IllegalStateException when the board starts with any non-zero value
+  // */
+  // public int countSolutions(){}
 
 }
