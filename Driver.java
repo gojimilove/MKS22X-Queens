3e64b338d09f04fee920044b448a490d14453d60
Jessica Kim <jkim00@stuy.edu>
2019-02-10 18:44:38 -0500

    tests for all positions on board

--- Driver.java
+++ Driver.java
@@ -2,9 +2,17 @@ public class Driver {
   public static void main(String[] args) {
     QueenBoard a = new QueenBoard(8);
     System.out.println(a);
-    a.addQueen(5,5);
+    // for (int i = 0; i < 8; i++) {
+    // 	for (int j = 0; j < 8; j++) {
+    // 		a.addQueen(i,j);
+    // 		System.out.println(a);
+    // 		a.removeQueen(i,j);
+    // 	}
+    // }
+    a.addQueen(0,0);
     System.out.println(a);
-    a.removeQueen(5,5);
+    a.removeQueen(0,0);
     System.out.println(a);
+    a.solve();
   }
 }
